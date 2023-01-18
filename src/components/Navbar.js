db25aecfee2970ac1ef7f9d67397ab04e85dd1fb
samcms1234 <samdevpro99@gmail.com>
2023-01-19 03:09:11 +0530

    Navbar elements

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../assets/logo.svg'
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" to="/">OpenData</Link>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item active">
-            <Link className="nav-link" to="/">Home</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/consent">Consent</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/data">Data</Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <nav class="bg-white fixed top-0 left-0 right-0 z-10 py-2 px-6 flex justify-between items-center">
+    <a href="#" class="text-lg font-medium text-gray-900">
+      <img src={logo} width='150px' height='250px'/>
+    </a>
+  <div class="flex justify-center md:justify-start">
+    <a href="#" class="px-3 py-1 rounded-md text-sm font-medium text-gray-600 hover:text-gray-900 focus:outline-none focus:text-gray-900 mr-3">
+      Give Consent
+    </a>
+    <a href="#" class="px-3 py-1 rounded-md text-sm font-medium text-gray-600 hover:text-gray-900 focus:outline-none focus:text-gray-900">
+      Check Consent
+    </a>
+    <a href="#" class="px-3 py-1 rounded-md text-sm font-medium text-gray-600 hover:text-gray-900 focus:outline-none focus:text-gray-900">
+      View my Permission
+    </a>
+  </div>
+  <div class="flex items-center justify-center md:justify-end">
+    <a href="#" class="px-3 py-1 rounded-md text-sm font-medium bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:bg-blue-600 mr-3">
+        Connect
+    </a>
+    <a href="#" class="px-3 py-1 rounded-md text-sm font-medium bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:bg-blue-600">
+      Login
+    </a>
+  </div>
+</nav>
+
   );
 };
 
