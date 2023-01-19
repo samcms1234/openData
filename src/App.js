fc6d374421721fa8ed3e4ce03512bd3c963394fb
samcms1234 <samdevpro99@gmail.com>
2023-01-19 11:43:46 +0530

    integrated Navbar Body and footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import Body from './components/Body';
+import Footer from './components/Footer';
+import Footer2 from './components/Footer2';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+
+      <Body />
+
+      <Footer2 />
     </div>
   );
 }
