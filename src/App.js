b529d97c22b0ddc573cac34d7254763edbeeaddf
Saumya Srivastava <89307999+samcms1234@users.noreply.github.com>
2023-05-13 12:20:11 +0000

    added user authentication components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React, { useState } from 'react';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
+import { LoginContext } from './contexts/LoginContext';
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import { Home, GiveConsent, CheckConsent, RevokeConsent } from './pages';
 
+import SignIn from './components/SignIn/SignIn';
+import SignUp from './components/SignUp/SignUp';
+
 function App() {
   const [view, setView] = useState("home");
+  const [loggedIn, setLoggedIn] = useState(false);
+
   console.log(window.web3);
 
   return (
@@ -19,6 +25,8 @@ function App() {
           <Route path="/giveconsent" element={<GiveConsent />} />
           <Route path="/checkconsent" element={<CheckConsent />} />
           <Route path="/revokeconsent" element={<RevokeConsent />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
         </Routes>
       </Router>
     </div>
---
+++ src/components/SignIn/SignIn.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import "./SignIn.css";
+
+const SignIn = () => {
+  return (
+    <div>SignIn</div>
+  )
+}
+
+export default SignIn
