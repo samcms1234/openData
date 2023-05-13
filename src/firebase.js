640070dd45175188e5efb064258b81db3d2ab355
Saumya Srivastava <89307999+samcms1234@users.noreply.github.com>
2023-05-13 12:18:56 +0000

    initiated firebase authentication

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDPa1iJ0wq7OeWv_0cAYH8o9k_peesWrtA",
+  authDomain: "opendata-1f068.firebaseapp.com",
+  projectId: "opendata-1f068",
+  storageBucket: "opendata-1f068.appspot.com",
+  messagingSenderId: "875252402926",
+  appId: "1:875252402926:web:c2295fdec5a24f857b01ff",
+  measurementId: "G-3MQKJ7M8L2"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth();
+
+export { app, auth };
