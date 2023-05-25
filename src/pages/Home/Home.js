eb4797526cbd1dad17301a112ffa302ed99a15dc
samcms1234 <samdevpro99@gmail.com>
2023-05-26 01:01:54 +0530

    removed signup page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,7 +4,7 @@ import bg from '../../assets/bg.png'
 import Navbar from '../../components/Navbar/Navbar'
 import Footer from '../../components/Footer/Footer'
 import { Link } from 'react-router-dom'
-import SignUp from '../../components/SignUp/SignUp'
+import './Home.css';
 
 const Home = () => {
   return (
@@ -14,17 +14,17 @@ const Home = () => {
         <div class="bg-cover bg-center h-screen w-screen" style={{ backgroundImage: `url(${bg})`}}>
         <div class="container mx-auto p-8 flex flex-col items-center justify-center h-full">
             <div class="container text-white text-center mt-10">
-                <h1 class="text-5xl font-medium">Consent Management made easy</h1>
-                <p class="text-xl mt-6">Easily manage and track your consents</p>
+                <h1 class="heading text-5xl font-medium">Consent Management made easy</h1>
+                <p class="heading text-xl mt-6">Easily manage and track your consents</p>
             </div>
 
             <div class="mt-40 container text-white text-center w-2/5 rounded-2xl border-2 border-gray-800 p-10">
-                <p className="text-gray-800 text-xl leading-relaxed">
+                <p className="heading text-gray-800 text-xl leading-relaxed">
                   OpenData is a platform that allows users to take control of their personal data. With OpenData, users can easily give and manage their consent for data sharing with complete transparency and security.
                 </p>
               <div className="mt-8">
                <button>
-                <Link to="/signup" className='bg-red-500 text-white p-3 rounded-md hover:bg-red-600'>
+                <Link to="/signin" className='heading bg-red-500 text-white p-3 rounded-md hover:bg-red-600'>
                   Get started
                 </Link>
                </button>
